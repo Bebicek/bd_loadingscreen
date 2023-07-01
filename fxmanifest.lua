376403c0b86733c508628896bfad63b48060dcaa
Bebicek <pepekga@seznam.cz>
2023-07-01 23:30:26 +0200

    První commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'digitalranger'
+description 'Discord : https://discord.gg/DW7m5aNGhK'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+    'logo/logo.png',
+    'song/song.mp3',
+    'video/video.mp4'
+}
