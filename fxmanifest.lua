cc1543cbec39931a4f1fd71e62a7a09c561ff6c8
Bebicek <pepekga@seznam.cz>
2023-07-09 15:18:12 +0200

    zmena komentare

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'Bebik(digitalranger)'
-description 'Discord : https://discord.gg/DW7m5aNGhK'
+description 'Discord : https://discord.gg/nunBYPBWFF'
 version '1.0.0'
 
 loadscreen 'index.html'
